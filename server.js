a39ae4326c138438bd5deae7a4c39540a5e1a12b
chikechris <cchike2017@gmail.com>
2019-09-09 15:18:25 -0400

    imported accounts, account-router to server.js

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
+const AcctRouter = require('./accounts/account-router')
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/account', AcctRouter)
+
+server.use('/', (req, res) => {
+  res.send('<h1>Web dataBase Page</h1>')
+})
 
 module.exports = server;
